b13baa6be75a0df133861a85a3db9677b6e84a2a
Nicholai Venetucci <nvenetucci@gmail.com>
2021-06-26 13:30:53 -0700

    add scraper API endpoint with Cheerio and Async/Await

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 const express = require("express");
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/api/cars/:make", (req, res) => {
+  const fetchResults = async () => {
+    try {
+      const response = await fetch(
+        `https://portland.craigslist.org/d/cars-trucks-by-owner/search/cto?nearbyArea=2&nearbyArea=232&nearbyArea=233&nearbyArea=246&nearbyArea=350&nearbyArea=94&query=${req.params.make}&searchNearby=2&sort=date`
+      );
+      const html = await response.text();
+      const $ = cheerio.load(html);
+      const results = [];
+
+      $(".result-info").each((i, el) => {
+        const title = $(el).find(".result-heading").text().trim();
+
+        results.push(title);
+      });
+
+      res.json(results);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  fetchResults();
 });
 
 const PORT = process.env.PORT || 5000;
