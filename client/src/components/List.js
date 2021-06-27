f5b0357d3451c72736a228f30f4c93799c5226cc
Nicholai Venetucci <nvenetucci@gmail.com>
2021-06-27 02:17:33 -0700

    add dropdown to search for auto parts

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 
 const List = () => {
+  const [category, setCategory] = useState("cars");
   const [results, setResults] = useState([]);
 
   useEffect(() => {
     const fetchResults = async () => {
       try {
-        const response = await fetch("/api/cars/bmw");
+        const response = await fetch(`/api/${category}/bmw`);
         const data = await response.json();
         setResults(data);
       } catch (err) {
@@ -15,27 +16,36 @@ const List = () => {
     };
 
     fetchResults();
-  }, []);
+  }, [category]);
 
   return (
-    <ul className="rows">
-      {results.map((result, index) => (
-        <li className="result-row" key={index}>
-          <div className="result-info">
-            <time className="result-time">{result.time}</time>
-            <h1 className="result-heading">
-              <a className="result-link" href={result.link}>
-                {result.title}
-              </a>
-            </h1>
-            <span className="result-meta">
-              <span className="result-price">{result.price}</span>
-              <span className="result-location">{result.location}</span>
-            </span>
-          </div>
-        </li>
-      ))}
-    </ul>
+    <>
+      {/* Category Dropdown Menu */}
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="cars">Cars</option>
+        <option value="parts">Parts</option>
+      </select>
+
+      {/* List Results */}
+      <ul className="rows">
+        {results.map((result, index) => (
+          <li className="result-row" key={index}>
+            <div className="result-info">
+              <span className="result-time">{result.time}</span>
+              <h1 className="result-heading">
+                <a className="result-link" href={result.link}>
+                  {result.title}
+                </a>
+              </h1>
+              <span className="result-meta">
+                <span className="result-price">{result.price}</span>
+                <span className="result-location">{result.location}</span>
+              </span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
--- server.js
+++ server.js
@@ -4,11 +4,57 @@ const cheerio = require("cheerio");
 
 const app = express();
 
+// Cars
 app.get("/api/cars/:make", (req, res) => {
   const fetchResults = async () => {
     try {
       const response = await fetch(
         `https://portland.craigslist.org/d/cars-trucks-by-owner/search/cto?nearbyArea=2&nearbyArea=232&nearbyArea=233&nearbyArea=246&nearbyArea=350&nearbyArea=94&query=${req.params.make}&searchNearby=2&sort=date`
+      );
+      const html = await response.text();
+      const $ = cheerio.load(html);
+      const results = [];
+
+      $(".result-info").each((i, el) => {
+        const time1 = $(el)
+          .find(".result-date")
+          .attr("title")
+          .split(" ")[3]
+          .split(":")
+          .slice(0, 2)
+          .join(":");
+        const time2 = $(el).find(".result-date").attr("title").split(" ")[4];
+        const link = $(el).find("a").attr("href");
+        const title = $(el).find(".result-heading").text().trim();
+        const price = $(el).find(".result-price").text();
+        const nearby = $(el).find(".nearby").text();
+        const location =
+          nearby !== "" ? nearby : $(el).find(".result-hood").text().trim();
+
+        results.push({
+          time: time1 + " " + time2,
+          link,
+          title,
+          price,
+          location,
+        });
+      });
+
+      res.json(results);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  fetchResults();
+});
+
+// Parts
+app.get("/api/parts/:make", (req, res) => {
+  const fetchResults = async () => {
+    try {
+      const response = await fetch(
+        `https://portland.craigslist.org/d/auto-parts-by-owner/search/pts?nearbyArea=2&nearbyArea=232&nearbyArea=233&nearbyArea=246&nearbyArea=350&nearbyArea=94&query=${req.params.make}&searchNearby=2&sort=date`
       );
       const html = await response.text();
       const $ = cheerio.load(html);
